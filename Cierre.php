5c1368840d1bcabd94966e310c750e860eb2aad9
Dr. Jose Edgar Lugo Castro <lugouadec@gmail.com>
2021-02-23 21:43:07 -0600

    Final

--- Cierre.php
+++ Cierre.php
@@ -61,7 +61,7 @@ if(!$ejecutarInsertar){
 		echo "Error en la linea de SQL";
 	}
 }
-mysqli_close($enlace);
+//mysqli_close($enlace);
 
 ?>
 
@@ -89,71 +89,238 @@ mysqli_close($enlace);
 			<tr>
 				<td>
 					<?php
-						$consultaIACind="SELECT $IAC_Ind FROM IAC";
-						if($IAC_Ind<25.0){
-						echo "Tu agotamiento Individual es leve";
+						$sql = "SELECT ((c1+c2+c3+c4+c5+c6)/6) prom FROM iac	WHERE consentimiento_folio=$folio;";
+						$IAC_Ind = 0; 
+						$resultado = mysqli_query($enlace,$sql);
+						if (!$resultado) {
+							// ¡Oh, no! La consulta falló. 
+							echo "Lo sentimos, este sitio web está experimentando problemas.";
 						}
-						if($IAC_Ind>75.0){
+						else{
+							mysqli_data_seek ($resultado, 0);
+							$extraido= mysqli_fetch_array($resultado);
+							// echo "- Folio: ".$extraido['folio']."<br/>";
+							// echo "- Fecha: ".$extraido['fecha']."<br/>";
+							if(!empty($extraido['prom'])){
+								$IAC_Ind = $extraido['prom'];
+							}
+						}
+						
+						echo $IAC_Ind."<br/>";
+
+						if($IAC_Ind>=25.0 && $IAC_Ind<=75.0){
+							echo "Tu agotamiento Individual es Moderado";
+						}elseif($IAC_Ind>75.0){
 						echo "Tu agotamiento Individual es alto, se te recomienda acercarte a la autoridad correspondiente";
-						} else {
-							echo "Tu agotamiento Trabajo es moderado";
+						}elseif($IAC_Ind<25.0) {
+							echo "Tu agotamiento Individual es leve";
 						}
 					?>
 			</tr>
 				</td>
 				<td>
 					<?php
-						$consultaIACtra="SELECT $IAC_tra FROM IAC";
-						if($IAC_tra<25.0){
-						echo "Tu agotamiento en Trabajo es leve";
+						$sql = "SELECT ((c7+c8+c9+c10+c11+c12+c13)/7) prom FROM iac	WHERE consentimiento_folio=$folio;";
+						$IAC_Ind = 0; 
+						$resultado = mysqli_query($enlace,$sql);
+						if (!$resultado) {
+							// ¡Oh, no! La consulta falló. 
+								echo "Lo sentimos, este sitio web está experimentando problemas.";
 						}
-						if($IAC_tra>75.0){
+						else{
+							mysqli_data_seek ($resultado, 0);
+							$extraido= mysqli_fetch_array($resultado);
+							if(!empty($extraido['prom'])){
+								$IAC_Ind = $extraido['prom'];
+							}
+						}
+						
+						//$consultaIACtra="SELECT $IAC_tra FROM IAC";
+						echo $IAC_Ind."<br/>";
+
+						if($IAC_Ind>=25.0 && $IAC_Ind<=75.0){
+							echo "Tu agotamiento Trabajo es Moderado";
+						}elseif($IAC_Ind>75.0){
 						echo "Tu agotamiento Trabajo es alto, se te recomienda acercarte a la autoridad correspondiente";
-						} else {
-							echo "Tu agotamiento Trabajo es moderado";
+						}elseif($IAC_Ind<25.0) {
+							echo "Tu agotamiento Trabajo es leve";
 						}
 					?>
 				</td>
 			<tr>
 				<td>
 					<?php
-						$consultaIACaten="SELECT $IAC_aten FROM IAC";
-						if($IAC_aten<25.0){
-						echo "Tu agotamiento en atencion es leve";
+						$sql = "SELECT ((c14+c15+c16+c17+c18+c19)/6) prom FROM iac	WHERE consentimiento_folio=$folio;";
+						$IAC_Ind = 0; 
+						$resultado = mysqli_query($enlace,$sql);
+						if (!$resultado) {
+							// ¡Oh, no! La consulta falló. 
+							echo "Lo sentimos, este sitio web está experimentando problemas.";
 						}
-						if($IAC_aten>75.0){
-						echo "Tu agotamiento en atencion es alto, se te recomienda acercarte a la autoridad correspondiente";
-						} else {
-							echo "Tu agotamiento en atencion es moderado";
+
+						else{
+								mysqli_data_seek ($resultado, 0);
+								$extraido= mysqli_fetch_array($resultado);
+								if(!empty($extraido['prom'])){
+									$IAC_Ind = $extraido['prom'];
+								}
+							}
+
+						
+						echo $IAC_Ind."<br/>";
+						if($IAC_Ind>=25.0 && $IAC_Ind<=75.0){
+							echo "Tu agotamiento Atencion es Moderado";
+						}elseif($IAC_Ind>75.0){
+							echo "Tu agotamiento Atencion es alto, se te recomienda acercarte a la autoridad correspondiente";
+						}elseif($IAC_Ind<25.0) {
+							echo "Tu agotamiento Atencion es leve";
 						}
+
+
+						// $consultaIACaten="SELECT $IAC_aten FROM IAC";
+						// if($IAC_aten<25.0){
+						// echo "Tu agotamiento en atencion es leve";
+						// }
+						// if($IAC_aten>75.0){
+						// echo "Tu agotamiento en atencion es alto, se te recomienda acercarte a la autoridad correspondiente";
+						// } else {
+						// 	echo "Tu agotamiento en atencion es moderado";
+						// }
 					?>
 				</td>
 			</tr>
 				<td>
 					<?php
-						$consultaEstres="SELECT $Final_estres FROM GWEstres";
-						if($Final_estres<25.0){
-						echo "Tu estres en estres laboral es leve";
+						$sql = "SELECT ((e1+e2+e3+e4+e5+e6+e7+e8+e9)/9) prom FROM gwestres	WHERE consentimiento_folio=$folio;";
+						$Final_estres = 0; 
+						$resultado = mysqli_query($enlace,$sql);
+						if (!$resultado) {
+							// ¡Oh, no! La consulta falló. 
+							echo "Lo sentimos, este sitio web está experimentando problemas.";
 						}
-						if($Final_estres>75.0){
-						echo "Tu agotamiento estres laboral es alto, se te recomienda acercarte a la autoridad correspondiente";
-						} else {
-							echo "Tu agotamiento estres laboral es moderado";
+
+						else{
+								mysqli_data_seek ($resultado, 0);
+								$extraido= mysqli_fetch_array($resultado);
+								if(!empty($extraido['prom'])){
+									$Final_estres = $extraido['prom'];
+								}
+							}
+
+						
+						echo $Final_estres."<br/>";
+						if($Final_estres>=25.0 && $Final_estres<=74.90){
+							echo "Estres Laboral es Moderado";
+						}elseif($Final_estres>75.0){
+							echo "Estres Laboral es alto, se te recomienda acercarte a la autoridad correspondiente";
+						}elseif($Final_estres<25.0) {
+							echo "Estres Laboral es leve";
 						}
+											
+						// $consultaEstres="SELECT $Final_estres FROM GWEstres";
+						// if($Final_estres<25.0){
+						// echo "Tu estres en estres laboral es leve";
+						// }
+						// if($Final_estres>75.0){
+						// echo "Tu agotamiento estres laboral es alto, se te recomienda acercarte a la autoridad correspondiente";
+						// } else {
+						// 	echo "Tu agotamiento estres laboral es moderado";
+						// }
 					?>
 				</td>
 			<tr>
 				<td>
 					<?php
-						$consultaExigencia="SELECT $Final_exigencia FROM exigencia";
-						if($Final_exigencia<25.0){
-						echo "Tu estres en estres laboral es leve";
+						$sql = "SELECT (p1+p3+p9+p11+p18+p19+p21+p23) suma FROM personalidad WHERE consentimiento_folio=$folio;";
+						$Final_Pneurotico = 0; 
+						$resultado = mysqli_query($enlace,$sql);
+						if (!$resultado) {
+							// ¡Oh, no! La consulta falló. 
+							echo "Lo sentimos, este sitio web está experimentando problemas.";
 						}
-						if($Final_exigencia>75.0){
-						echo "Tu estres laboral es alto, se te recomienda acercarte a la autoridad correspondiente";
-						} else {
-							echo "Tu agotamiento estres laboral es moderado";
+
+						else{
+								mysqli_data_seek ($resultado, 0);
+								$extraido= mysqli_fetch_array($resultado);
+								if(!empty($extraido['suma'])){
+									$Final_Pneurotico = $extraido['suma'];
+								}
+							}
+
+						
+						echo $Final_Pneurotico."<br/>";
+						if($Final_Pneurotico>=8.0 && $Final_Pneurotico<=29){
+							echo "Personalidad Neurotica Baja";
+						}elseif($Final_Pneurotico>=30  && $Final_Pneurotico <= 50){
+							echo "Personalidad Neurotica Promedio";
+						}elseif($Final_Pneurotico>50) {
+							echo "Personalidad Neurotica Alta";
 						}
+
+						echo "<br />";
+
+						$sql = "SELECT (p2+p8+p10+p13+p14+p16+p20+p24) suma FROM personalidad WHERE consentimiento_folio=$folio;";
+						$Final_Pneurotico = 0; 
+						$resultado = mysqli_query($enlace,$sql);
+						if (!$resultado) {
+							// ¡Oh, no! La consulta falló. 
+							echo "Lo sentimos, este sitio web está experimentando problemas.";
+						}
+
+						else{
+								mysqli_data_seek ($resultado, 0);
+								$extraido= mysqli_fetch_array($resultado);
+								if(!empty($extraido['suma'])){
+									$Final_Pneurotico = $extraido['suma'];
+								}
+							}
+
+						
+						echo $Final_Pneurotico."<br/>";
+						if($Final_Pneurotico>=8.0 && $Final_Pneurotico<=29){
+							echo "Personalidad Neurotica Baja";
+						}elseif($Final_Pneurotico>=30  && $Final_Pneurotico <= 50){
+							echo "Personalidad Neurotica Promedio";
+						}elseif($Final_Pneurotico>50) {
+							echo "Personalidad Neurotica Alta";
+						}
+						
+						echo "<br />";
+
+						$sql = "SELECT (p4+p5+p6+p7+p12+p15+p17+p22) suma FROM personalidad WHERE consentimiento_folio=$folio;";
+						$Final_Pneurotico = 0; 
+						$resultado = mysqli_query($enlace,$sql);
+						if (!$resultado) {
+							// ¡Oh, no! La consulta falló. 
+							echo "Lo sentimos, este sitio web está experimentando problemas.";
+						}
+
+						else{
+								mysqli_data_seek ($resultado, 0);
+								$extraido= mysqli_fetch_array($resultado);
+								if(!empty($extraido['suma'])){
+									$Final_Pneurotico = $extraido['suma'];
+								}
+							}
+
+						
+						echo $Final_Pneurotico."<br/>";
+						if($Final_Pneurotico>=8.0 && $Final_Pneurotico<=29){
+							echo "Personalidad Neurotica Baja";
+						}elseif($Final_Pneurotico>=30  && $Final_Pneurotico <= 50){
+							echo "Personalidad Neurotica Promedio";
+						}elseif($Final_Pneurotico>50) {
+							echo "Personalidad Neurotica Alta";
+						}
+						// $consultaExigencia="SELECT $Final_exigencia FROM exigencia";
+						// if($Final_exigencia<25.0){
+						// echo "Tu estres en estres laboral es leve";
+						// }
+						// if($Final_exigencia>75.0){
+						// echo "Tu estres laboral es alto, se te recomienda acercarte a la autoridad correspondiente";
+						// } else {
+						// 	echo "Tu agotamiento estres laboral es moderado";
+						// }
 					?>
 				</td>
 			</tr>
@@ -218,6 +385,8 @@ mysqli_close($enlace);
 	<br>
 	<br>
 	<br>
-
+<?php
+	mysqli_close($enlace);
+?>
 </body>
 </html>
